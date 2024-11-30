6540a92f8e2074043a8770fbeff60bcaa72059a4
RevainIDN <kov.igor94@gmail.com>
2024-11-30 22:49:00 +0200

    Создал отдельные папки: для компонентов и стилей

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import '../styles/App.css'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>HELLO WORLD</h1>
     </>
   )
 }
