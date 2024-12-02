d1b115ccd726274d85a2d83864c4ffa870f31317
RevainIDN <kov.igor94@gmail.com>
2024-12-02 20:53:20 +0200

    Реализовано получение данных с 'сервера' и использование этих данных в переиспользуемом компоненте

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,27 @@
 import '../styles/Card.css';
 
-export default function Card() {
+export default function Card({ name, image, price, rating, votes, popular, available }) {
 	return (
 		<>
 			<div className='card'>
-				<div className='popular'>Popular</div>
-				<img className='image' src="https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/cappuccino.jpg" alt="" />
+				{popular && <div className='popular'>Popular</div>}
+				<img className='image' src={image} alt={name} />
 				<div className='product-info'>
-					<h1 className='product-title'>Cappuccino</h1>
-					<p className='price'><small>$5.20</small></p>
+					<h1 className='product-title'>{name}</h1>
+					<p className='price'><small>{price}</small></p>
 				</div>
-				<div className='product-rate'>
-					<img className='star' src="/public/Star_fill.svg" alt="Star" />
-					<p className='rating'>4.7</p>
-					<p className='votes'><small>(65 votes)</small></p>
-					<p className='sold'>Sold out</p>
-				</div>
-				<div className='product-no-rate'>
-					<img className='empty-star' src="/public/Star.svg" alt="Empty star" />
-					<p className='votes'><small>No ratings</small></p>
-				</div>
+				{votes >= 1 &&
+					<div className='product-rate'>
+						<img className='star' src="/public/Star_fill.svg" alt="Star" />
+						<p className='rating'>{rating}</p>
+						<p className='votes'><small>({votes} votes)</small></p>
+						{available === false && <p className='sold'>Sold out</p>}
+					</div>}
+				{votes === 0 &&
+					<div className='product-no-rate'>
+						<img className='empty-star' src="/public/Star.svg" alt="Empty star" />
+						<p className='votes'><small>No ratings</small></p>
+					</div>}
 			</div>
 		</>
 	)
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,21 +1,35 @@
 import '../styles/MainContent.css';
+import { useEffect, useState } from 'react';
 import Title from './Title';
 import Buttons from './Buttons';
 import Card from './Card';
 
 export default function MainContent() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json')
+				const data = await response.json();
+				setProducts(data);
+			} catch (error) {
+				console.error('Ошибка при загрузке данных:', error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<div className='main-content'>
 				<Title />
 				<Buttons />
-				<div className='cards'>
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+				<div className="cards">
+					{products.map(product => (
+						<Card key={product.id} {...product} />
+					))}
 				</div>
 			</div>
 		</>
